bc0ce1c086f9aafce4ad5a1839b5bacca020054b
Kuitos <kuitos.lau@gmail.com>
2019-05-30 20:12:09 +0800

    :sparkles: add yaml control

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,18 @@
  * @since 2017-11-03
  */
 
+import { readFile } from 'fs';
+import { load } from 'js-yaml';
 import { scheduleJob } from 'node-schedule';
+import * as path from 'path';
+import { promisify } from 'util';
 import start from './schedulers/recommends';
 
 console.info('app initialized!');
-// 每天7点执行数据抓取任务
-scheduleJob({hour: 7, minute: 0, dayOfWeek: [0, 1, 2, 3, 4, 5, 6]}, async () => {
 
+const readFilePromise: (path: string, encoding: string) => Promise<string> = promisify(readFile);
+
+async function startJob() {
 	try {
 
 		console.info('data mining job start...');
@@ -20,5 +25,17 @@ scheduleJob({hour: 7, minute: 0, dayOfWeek: [0, 1, 2, 3, 4, 5, 6]}, async () => 
 	} catch (e) {
 		console.error('error occurs when job executing', e);
 	}
-});
+}
 
+(async () => {
+	const metadata = load(await readFilePromise(path.join(__dirname, '../metadata.yml'), 'utf-8'));
+
+	const { fireImmediately } = metadata;
+	if (fireImmediately) {
+		console.info('job start immediately ----->');
+		await startJob();
+	}
+})();
+
+// 每天7点执行数据抓取任务
+scheduleJob({ hour: 7, minute: 0, dayOfWeek: [0, 1, 2, 3, 4, 5, 6] }, startJob);
--- src/schedulers/recommends.ts
+++ src/schedulers/recommends.ts
@@ -3,7 +3,11 @@
  * @homepage https://github.com/kuitos/
  * @since 2017-11-13
  */
+import { readFile } from 'fs';
+import { load } from 'js-yaml';
 import { intersectionBy, merge } from 'lodash';
+import * as path from 'path';
+import { promisify } from 'util';
 import { IBaseFund } from '../interfaces/BaseFund';
 import { IFund } from '../interfaces/Fund';
 import { IRatingFund } from '../interfaces/RatingFund';
@@ -11,13 +15,16 @@ import { fetchFunds } from '../loaders/fetchFunds';
 import { fetchAllRatings } from '../loaders/fetchRatings';
 import dao from '../utils/dao';
 
+const readFilePromise: (path: string, encoding: string) => Promise<string> = promisify(readFile);
+
 interface IndexedFund {
 	[name: string]: IFund;
 }
 
 export default async function start() {
 
-	const sampleSize = 500;
+	const metadata = load(await readFilePromise(path.join(__dirname, '../../metadata.yml'), 'utf-8'));
+	const { sampleSize } = metadata;
 
 	const collection = await Promise.all([
 		fetchFunds('6yzf', sampleSize),
@@ -29,8 +36,9 @@ export default async function start() {
 
 	// 根据基金 no 做交集处理
 	const [, , , , ratings] = collection;
+	// @ts-ignore
 	const intersectedFunds: Array<IBaseFund & IRatingFund> = intersectionBy.apply(null, [...collection, 'no'])
-		.map((v: IFund) => merge(v, ratings.find(rate => rate.no === v.no) || {mstar: 0}))
+		.map((v: IFund) => merge(v, ratings.find(rate => rate.no === v.no) || { mstar: 0 }))
 		.filter((v: any) => v.perfectCount >= 2);
 
 	const indexedFunds = intersectedFunds.reduce((a: IndexedFund, b: IFund) => {
@@ -39,7 +47,7 @@ export default async function start() {
 	}, {});
 
 	await dao.add('/recommendFunds', indexedFunds);
-	await dao.update('/recommendFunds', {length: intersectedFunds.length});
+	await dao.update('/recommendFunds', { length: intersectedFunds.length });
 
 	console.info(`${intersectedFunds.length} recommended funds saved!`);
 }
